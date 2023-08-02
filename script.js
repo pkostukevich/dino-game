f84813ae905c45de360222b6ff24019484e16fb6
Polina Kostukevich <kostukevich.p9@gmail.com>
2023-08-02 13:22:49 +0300

    add bird func

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+const bird = document.getElementById("bird");
 
 document.addEventListener("keydown", function(event){
     jump();
@@ -17,8 +18,12 @@ function jump (){
 let isAlive = setInterval( function(){
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    let birdLeft = parseInt(window.getComputedStyle(bird).getPropertyValue("left"));
 
-    if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+    if((cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140)||(birdLeft < 40 && birdLeft > 0 && dinoTop <= 130)){
         alert("GAME OVER!");
+        dino.style.top = "150px";
+        cactus.style.left = "620px";
+        bird.style.left = "620px";
     }
 }, 10);
